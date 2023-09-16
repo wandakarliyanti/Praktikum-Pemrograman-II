a5d83135a280494f5f9cff4a481f372290a6ab7b
Wanda Karliyanti <115330174+wandakarliyanti@users.noreply.github.com>
2023-09-17 01:03:59 +0800

    Add files via upload

---
+++ modul 1/PRAK104_2210817220034_WandaKarliyanti.java
@@ -0,0 +1,9 @@
+public class PRAK104_2210817220034_WandaKarliyanti {
+    public static void main(String[]args){
+        String nama="Wanda Karliyanti";
+        Integer umur=20;
+
+        System.out.print("Perkenalkan, Nama Saya " + nama + ", ");
+        System.out.print("Umur Saya " + umur);
+    }
+}
