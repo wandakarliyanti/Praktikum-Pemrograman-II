e4eaef4880d585064d2fa54d2749b0986d1f3f5e
Wanda Karliyanti <115330174+wandakarliyanti@users.noreply.github.com>
2023-09-18 17:53:39 +0800

    Add files via upload

---
+++ modul 1/PRAK102_2210817220034_WandaKarliyanti.java
@@ -0,0 +1,10 @@
+public class PRAK102_2210817220034_WandaKarliyanti {
+    public static void main(String[]args){
+        System.out.println("##############################");
+        System.out.println("#                            #");
+        System.out.println("#      Wanda Karliyanti      #");
+        System.out.println("#        2210817220034       #");
+        System.out.println("#                            #");
+        System.out.print("##############################");
+    }
+}
---
+++ modul 1/PRAK103_2210817220034_WandaKarliyanti.java
@@ -0,0 +1,11 @@
+public class PRAK103_2210817220034_WandaKarliyanti {
+    public static void main(String[]args){
+        System.out.print("###                     ###    #####\n" +
+        " ###                   ###      ###\n" +
+        "  ###                 ###       ###\n" +
+        "   ###      ###      ###        ###\n" +
+        "    ###    #####    ###         ###\n" +
+        "     ### ###   ### ###          ###\n" +
+        "      #####     #####          #####");
+    }
+}
