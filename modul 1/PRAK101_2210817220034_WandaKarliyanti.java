15e96e641c0ce244cb6269cc0122cd50fc32dcfe
Wanda Karliyanti <115330174+wandakarliyanti@users.noreply.github.com>
2023-09-18 17:53:39 +0800

    Add files via upload

---
+++ modul 1/PRAK101_2210817220034_WandaKarliyanti.java
@@ -0,0 +1,7 @@
+public class PRAK101_2210817220034_WandaKarliyanti {
+    public static void main(String[]args){
+        System.out.println("Selamat Pagi, Wanda Karliyanti");
+        System.out.println("Selamat Siang, Wanda Karliyanti");
+        System.out.print("Selamat Malam, Wanda Karliyanti");
+    }
+}
