a0ac71a6cbb5bd53d7214b15ca6b888f49c6065e
Wanda Karliyanti <115330174+wandakarliyanti@users.noreply.github.com>
2023-09-18 18:06:39 +0800

    Update PRAK105_2210817220034_WandaKarliyanti.java

--- modul 1/PRAK105_2210817220034_WandaKarliyanti.java
+++ modul 1/PRAK105_2210817220034_WandaKarliyanti.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class PRAK105_2210817220034_WandaKarliyanti {
     public static void main(String[]args){
         String makanan, hobi;
-        Scanner keyboard=new Scanner(System.in);
+        Scanner isi=new Scanner(System.in);
 
         System.out.print("Masukan Makanan Favorit: ");
-            makanan= keyboard.nextLine();
+            makanan= isi.nextLine();
         System.out.print("Masukan Hobi: ");
-            hobi= keyboard.nextLine();
+            hobi= isi.nextLine();
 
         System.out.print("\nAku Suka Makan "+ makanan + ", ");
         System.out.print("dan Hobiku " + hobi);
