c1010d17eb01551902536044f2d3e68b318904df
Wanda Karliyanti <115330174+wandakarliyanti@users.noreply.github.com>
2023-09-18 17:51:30 +0800

    Delete modul 1 directory

--- modul 1/PRAK104_2210817220034_WandaKarliyanti.java
+++
@@ -1,9 +0,0 @@
-public class PRAK104_2210817220034_WandaKarliyanti {
-    public static void main(String[]args){
-        String nama="Wanda Karliyanti";
-        Integer umur=20;
-
-        System.out.print("Perkenalkan, Nama Saya " + nama + ", ");
-        System.out.print("Umur Saya " + umur);
-    }
-}
