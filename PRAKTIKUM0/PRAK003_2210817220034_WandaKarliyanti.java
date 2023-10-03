3c8a9f6ee6f4eac532592e4718df7c852709ca5f
Wanda Karliyanti <2210817220034@mhs.ulm.ac.id>
2023-10-03 15:35:13 +0800

    UPLOAD WANDA

---
+++ PRAKTIKUM0/PRAK003_2210817220034_WandaKarliyanti.java
@@ -0,0 +1,11 @@
+public class PRAK003_2210817220034_WandaKarliyanti {
+    public static void main(String[]args){
+        System.out.print("###                     ###    #####\n" +
+        " ###                   ###      ###\n" +
+        "  ###                 ###       ###\n" +
+        "   ###      ###      ###        ###\n" +
+        "    ###    #####    ###         ###\n" +
+        "     ### ###   ### ###          ###\n" +
+        "      #####     #####          #####");
+    }
+}
