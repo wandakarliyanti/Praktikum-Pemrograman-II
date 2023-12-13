c104e6a70e32770b9fb1101d695d69cba46fc38a
Wanda Karliyanti <115330174+wandakarliyanti@users.noreply.github.com>
2023-12-13 09:19:11 +0800

    Delete PRAKTIKUM6 directory

--- PRAKTIKUM6/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.praktikum6 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.praktikum6 to javafx.fxml;
-    exports com.example.praktikum6;
-}
